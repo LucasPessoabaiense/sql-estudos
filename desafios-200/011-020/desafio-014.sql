7d3b1e5ab2c5d935a0c1d95bc6e8cd84e2a01965
Lucas Pessôa Baiense <117696096+LucasPessoabaiense@users.noreply.github.com>
2026-09-19 16:09:37 -0300

    Desafio 014: SELECT de colunas específicas

---
+++ desafios-200/011-020/desafio-014.sql
@@ -0,0 +1,10 @@
+-- Desafio 014
+-- Pergunta: Nome, cidade e estado dos clientes
+-- Conceito: SELECT de colunas específicas
+-- Resultado: 90 linhas
+
+SELECT
+    nome_cliente,
+    cidade,
+    estado
+FROM clientes;
