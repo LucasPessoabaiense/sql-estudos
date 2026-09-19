92356315ca04a64eae3c1e27d0cb98234faf0352
Lucas Pessôa Baiense <117696096+LucasPessoabaiense@users.noreply.github.com>
2026-09-19 16:05:37 -0300

    Desafio 001: SELECT de colunas
    Create SQL query to list product details.

---
+++ desafios-200/001-010/desafio-001.sql
@@ -0,0 +1,10 @@
+-- Desafio 001
+-- Pergunta: Listar nome, preço e estoque de todos os produtos
+-- Conceito: SELECT de colunas específicas
+-- Resultado: 115 linhas
+
+SELECT
+    nome_produto,
+    preco,
+    estoque
+FROM produtos;
