9bcb943e9bdb0be449ba3f72dd6f7c487b3ffe3f
Lucas Pessôa Baiense <117696096+LucasPessoabaiense@users.noreply.github.com>
2026-09-19 16:06:09 -0300

    Desafio 002: SELECT de colunas

---
+++ desafios-200/001-010/desafio-002.sql
@@ -0,0 +1,10 @@
+-- Desafio 002
+-- Pergunta: Listar nome, cidade e estado dos clientes
+-- Conceito: SELECT de colunas específicas
+-- Resultado: 90 linhas
+
+SELECT
+    nome_cliente,
+    cidade,
+    estado
+FROM clientes;
