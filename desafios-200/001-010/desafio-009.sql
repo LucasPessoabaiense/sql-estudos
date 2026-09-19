800d8cd10699b964662a802616a6b0175e438843
Lucas Pessôa Baiense <117696096+LucasPessoabaiense@users.noreply.github.com>
2026-09-19 16:08:29 -0300

    Desafio 009: SELECT de colunas específicas

---
+++ desafios-200/001-010/desafio-009.sql
@@ -0,0 +1,10 @@
+-- Desafio 009
+-- Pergunta: Nome, preço e estoque dos produtos
+-- Conceito: SELECT de colunas específicas
+-- Resultado: 115 linhas
+
+SELECT
+    nome_produto AS nome,
+    preco,
+    estoque
+FROM produtos;
