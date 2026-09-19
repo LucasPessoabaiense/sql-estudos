b66fb7862839efc94d51cde458dbde0c73425344
Lucas Pessôa Baiense <117696096+LucasPessoabaiense@users.noreply.github.com>
2026-09-19 16:07:58 -0300

    Desafio 004: SELECT de colunas específicas

---
+++ desafios-200/001-010/desafio-004.sql
@@ -0,0 +1,10 @@
+-- Desafio 004
+-- Pergunta: Listar código, data e valor total das vendas
+-- Conceito: SELECT de colunas específicas
+-- Resultado: 160 linhas
+
+SELECT
+    id_venda,
+    data_venda,
+    valor_total
+FROM vendas;
