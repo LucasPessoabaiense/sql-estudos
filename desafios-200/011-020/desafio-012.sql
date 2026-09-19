2e89d78843b32d5af2e4a1f78687fc3e0f308685
Lucas Pessôa Baiense <117696096+LucasPessoabaiense@users.noreply.github.com>
2026-09-19 16:09:25 -0300

    Desafio 012: SELECT de colunas específicas

---
+++ desafios-200/011-020/desafio-012.sql
@@ -0,0 +1,9 @@
+-- Desafio 012
+-- Pergunta: Nome e cargo dos funcionários
+-- Conceito: SELECT de colunas específicas
+-- Resultado: 30 linhas
+
+SELECT
+    nome_funcionario,
+    cargo
+FROM funcionarios;
