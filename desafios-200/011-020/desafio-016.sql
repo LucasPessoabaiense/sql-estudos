ecf8878c91620eae9e1d9e951eacb56d8bf87859
Lucas Pessôa Baiense <117696096+LucasPessoabaiense@users.noreply.github.com>
2026-09-19 16:09:49 -0300

    Desafio 016: SELECT de colunas específicas

---
+++ desafios-200/011-020/desafio-016.sql
@@ -0,0 +1,11 @@
+-- Desafio 016
+-- Pergunta: IDs de venda, cliente, funcionário e valor total
+-- Conceito: SELECT de colunas específicas
+-- Resultado: 160 linhas
+
+SELECT
+    id_venda,
+    id_cliente,
+    id_funcionario,
+    valor_total
+FROM vendas;
