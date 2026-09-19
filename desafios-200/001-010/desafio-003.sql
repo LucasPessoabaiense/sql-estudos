dc83ead7f21e98e48e1dae374e37d4f1d8c1fc7a
Lucas Pessôa Baiense <117696096+LucasPessoabaiense@users.noreply.github.com>
2026-09-19 16:06:51 -0300

    Desafio 003: SELECT de colunas específicas

---
+++ desafios-200/001-010/desafio-003.sql
@@ -0,0 +1,10 @@
+-- Desafio 003
+-- Pergunta: Listar nome, cargo e salário dos funcionários
+-- Conceito: SELECT de colunas específicas
+-- Resultado: 30 linhas
+
+SELECT
+    nome_funcionario,
+    cargo,
+    salario
+FROM funcionarios;
