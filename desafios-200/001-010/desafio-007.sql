ff328a6f9114ef13d669e371c2d1a1cd58994c29
Lucas Pessôa Baiense <117696096+LucasPessoabaiense@users.noreply.github.com>
2026-09-19 16:08:17 -0300

    Desafio 007: SELECT de colunas específicas

---
+++ desafios-200/001-010/desafio-007.sql
@@ -0,0 +1,9 @@
+-- Desafio 007
+-- Pergunta: Código e nome das categorias
+-- Conceito: SELECT de colunas específicas
+-- Resultado: 15 linhas
+
+SELECT
+    id_categoria,
+    nome_categoria
+FROM categorias;
