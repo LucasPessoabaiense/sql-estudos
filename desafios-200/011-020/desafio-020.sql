958902b310d372a914e8a586bfddb8036c5d294a
Lucas Pessôa Baiense <117696096+LucasPessoabaiense@users.noreply.github.com>
2026-09-19 16:10:55 -0300

    Desafio 020: SELECT de colunas específicas

---
+++ desafios-200/011-020/desafio-020.sql
@@ -0,0 +1,9 @@
+-- Desafio 020
+-- Pergunta: Nome do cliente e data de cadastro
+-- Conceito: SELECT de colunas específicas
+-- Resultado: 90 linhas
+
+SELECT
+    nome_cliente,
+    data_cadastro
+FROM clientes;
