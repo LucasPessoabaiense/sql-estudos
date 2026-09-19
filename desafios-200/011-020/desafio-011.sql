e8f67a838e70d3babaa6d2122fa31aa0a3924650
Lucas Pessôa Baiense <117696096+LucasPessoabaiense@users.noreply.github.com>
2026-09-19 16:08:41 -0300

    Desafio 011: SELECT com alias (AS)

---
+++ desafios-200/011-020/desafio-011.sql
@@ -0,0 +1,9 @@
+-- Desafio 011
+-- Pergunta: Nome do cliente (como Cliente) e cidade
+-- Conceito: SELECT com alias (AS)
+-- Resultado: 90 linhas
+
+SELECT
+    nome_cliente AS Cliente,
+    cidade
+FROM clientes;
